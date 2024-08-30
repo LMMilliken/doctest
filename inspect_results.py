36ed7b91fc9261b93798c61d9a43ad855b21baf2
LMMilliken <louis@djmilliken.co.uk>
2024-08-30 14:09:50 +0900

    feat: visualising results

--- inspect_results.py
+++ inspect_results.py
@@ -71,9 +71,9 @@ def get_data(runs) -> str:
             "build_succ",
             "avg_tries",
             "avg_duration",
-            # "avg_retrieved",
-            # "avg_recall",
-            # "n_relevant",
+            "avg_retrieved",
+            "avg_recall",
+            "n_relevant",
             "build_status",
             "n_tries",
         ],
@@ -184,7 +184,7 @@ def get_repo_tags():
     return ret
 
 
-def group_by_tags(data: List[Any], tags: str, repo_tags: Dict[str, Any]):
+def group_by_tags(data: List[Any], tags: List[str], repo_tags: Dict[str, Any]):
     ret = []
     for tag in tags:
         ret.append(list(filter(lambda x: tag in repo_tags[x[0]]["tags"], data)))
@@ -220,30 +220,121 @@ def scatter(
     plt.show()
 
 
-def inspect(data, do_table: bool = False, do_scatter: bool = False):
+def multi_scatter(
+    x_data: List[List[Union[int, float]]],
+    y_data: List[List[Union[int, float]]],
+    group_labels: List[str],
+    x_label: str = "x",
+    y_label: str = "y",
+    title: str = "title",
+    lobf: bool = False,
+):
+    colors = plt.colormaps["Dark2"]
+    for i, (xd, yd, label) in enumerate(zip(x_data, y_data, group_labels)):
+        plt.scatter(xd, yd, color=colors(i), label=label)
+        if lobf:
+            x = np.array(xd)
+            y = np.array(yd)
+            m, b = np.polyfit(x, y, 1)
+            plt.plot(x, m * x + b, color=colors(i))
+    # plt.scatter(x_data, y_data)
+
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.show()
+
+
+def inspect(
+    data,
+    do_table: bool = False,
+    do_scatter: bool = False,
+    groups=None,
+    group_labels=None,
+    title=None,
+    lobf=True,
+):
     if do_table:
         print(array_to_markdown_table(data))
     if do_scatter:
-        recall = [row[5] for row in data[1:]]
-        retrieved = [row[4] for row in data[1:]]
-        build_rate = [
-            int(row[1].split("/")[0]) / int(row[1].split("/")[1]) for row in data[1:]
-        ]
-        scatter(recall, build_rate, "recall", "build_rate", "")
+        if groups is not None:
+            recall = [[row[5] for row in group] for group in groups]
+            build_rate = [
+                [int(row[1].split("/")[0]) / int(row[1].split("/")[1]) for row in group]
+                for group in groups
+            ]
+            multi_scatter(
+                recall,
+                build_rate,
+                group_labels,
+                "recall",
+                "build_rate",
+                title=f"results grouped by {title}" if title is not None else None,
+                lobf=lobf,
+            )
+        else:
+            recall = [row[5] for row in data[1:]]
+            retrieved = [row[4] for row in data[1:]]
+            build_rate = [
+                int(row[1].split("/")[0]) / int(row[1].split("/")[1])
+                for row in data[1:]
+            ]
+            scatter(recall, build_rate, "recall", "build_rate", "", lobf=lobf)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--run",
-    nargs="+",
-    default=["fraudulent-nidorina"],
-    help="path to the run to visualise",
-)
-args = parser.parse_args()
-data = get_data(args.run)
+try:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--run",
+        nargs="+",
+        default=["fraudulent-nidorina"],
+        help="path to the run to visualise",
+    )
+    parser.add_argument(
+        "--group",
+        action="store_true",
+        help="whether to group results when inspecting",
+    )
+    args = parser.parse_args()
+    run = args.run
+    do_group = args.group
+except:
+    run = ["fraudulent-nidorina"]
+    do_group = False
+data = get_data(run)
+print(len(data))
+repo_tags = get_repo_tags()
 groups = group_by_tags(data[1:], ["requirements", "poetry"], get_repo_tags())
-if __name__ == "__main__":
+group_sets = [["requirements", "poetry"], ["pytest", "unittest"]]
+group_data = [group_by_tags(data[1:], group, repo_tags) for group in group_sets]
+group_titles = ["installation method", "test method"]
 
-    inspect(data, do_table=True, do_scatter=True)
-    for group in groups:
-        inspect([data[0]] + group, do_scatter=True)
+if __name__ == "__main__":
+    if do_group:
+        for group, group_labels, group_title in zip(
+            group_data, group_sets, group_titles
+        ):
+            inspect(
+                data,
+                do_table=True,
+                do_scatter=True,
+                groups=group,
+                group_labels=group_labels + ["other"],
+                title=group_title,
+                lobf=True,
+            )
+            inspect(
+                data,
+                do_table=False,
+                do_scatter=True,
+                groups=group,
+                group_labels=group_labels + ["other"],
+                title=group_title,
+                lobf=False,
+            )
+    else:
+        inspect(data, do_table=True, do_scatter=True, lobf=True)
+# for group in groups:
+#     print(len(group))
+#     inspect([data[0]] + group, do_scatter=True)
